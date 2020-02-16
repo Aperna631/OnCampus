db46ab7e015a2b89ae4fbf9de83f9ddff856374a
Aperna631 <61126017+Aperna631@users.noreply.github.com>
2020-02-16 17:31:12 -0500

    Add files via upload

---
+++ OnCampus/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  OnCampus
+//
+//  Created by Dane Brazinski on 2/15/20.
+//  Copyright © 2020 Dane Brazinski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+  
+    @IBAction func aClick(_ sender: UIButton) {
+        print("Filter Pressed")
+        self.performSegue(withIdentifier: "thirdViewSegue", sender: self)
+        
+    }
+    func dropDown(){
+        
+    }
+    @IBAction func home(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "ogBack", sender: self)
+    }
+    
+    }
+
+
+
